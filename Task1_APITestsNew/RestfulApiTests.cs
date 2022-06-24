b717a3ba0eb927534460e2ecb823746f39a86d8c
User <User@DESKTOP-3CAS9B5>
2022-06-24 14:44:48 +0300

    Add project files.

---
+++ Task1_APITestsNew/RestfulApiTests.cs
@@ -0,0 +1,127 @@
+using RestSharp;
+using System.Net;
+using System.Text.Json;
+
+namespace TaskBoardRestfulApiTests
+{
+    public class RestfulApiTests
+    {
+        private RestClient client;
+
+        private const string baseUrl = "https://taskboard.nakov.repl.co/api/tasks";
+        private RestRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.client = new RestClient(baseUrl);
+        }
+
+        [Test]
+        public void Test_GetAllTasks_FirstTasksName()
+        {
+            // Arrange
+            this.request = new RestRequest(baseUrl);
+
+            // Act
+            var response = this.client.Execute(request);
+            var tasks = JsonSerializer.Deserialize<List<TaskBoard>>(response.Content);
+
+            // Assert
+            Assert.IsNotNull(response.Content);
+            Assert.IsTrue(tasks.Count > 0);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var isBoardFound = false;
+            foreach (var task in tasks)
+            {
+                if (task.board.name == "Done")
+                {
+                    isBoardFound = true;
+                    Assert.AreEqual("Project skeleton", task.title);
+                    break;
+                }
+            }
+
+            if (isBoardFound == false)
+            {
+                Assert.Fail("The Board is not found!");
+            }
+        }
+
+        [Test]
+        public void Test_FirstTasksKeyword_Valid()
+        {
+            // Arrange
+            request = new RestRequest(baseUrl + "/search/Home");
+
+            // Act
+            var response = client.Execute(request);
+            var tasks = JsonSerializer.Deserialize<List<TaskBoard>>(response.Content);
+
+            // Assert
+            Assert.IsNotNull(response.Content);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("Home page", tasks.First().title);
+        }
+
+        [Test]
+        public void Test_FirstTasksKeyword_Invalid()
+        {
+            // Arrange
+            var randnum = DateTime.Now.Ticks;
+            request = new RestRequest(baseUrl + "/search/" + randnum);
+
+            // Act
+            var response = client.Execute(request);
+            var tasks = JsonSerializer.Deserialize<List<TaskBoard>>(response.Content);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsTrue(tasks.Count == 0);
+        }
+
+        [Test]
+        public void Test_CreateTask_InvalidData()
+        {
+            // Arrange
+            var title = string.Empty;
+            var description = "API + UI tests";
+            var board = "Open";
+            request = new RestRequest(baseUrl);
+            request.AddJsonBody(new { title, description, board });
+            var response = client.Execute(request, Method.Post);
+
+            // Act
+            var tasks = JsonSerializer.Deserialize<TaskBoard>(response.Content);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.AreEqual("Title cannot be empty!", tasks.errMsg);
+        }
+
+        [Test]
+        public void Test_CreateTask_ValidData()
+        {
+            // Arrange
+            var title = "New Task" + DateTime.Now.Ticks;
+            var description = "API + UI tests";
+            var board = "Open";
+            request = new RestRequest(baseUrl);
+            request.AddJsonBody(new { title, description, board });
+            var response = client.Execute(request, Method.Post);
+
+            // Act
+            var newTask = JsonSerializer.Deserialize<TaskBoard>(response.Content);
+
+            request = new RestRequest(baseUrl + "/search/" + title);
+            var newesponse = client.Execute(request);
+            var tasks = JsonSerializer.Deserialize<List<TaskBoard>>(newesponse.Content);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Assert.AreEqual("Task added.", newTask.msg);
+            Assert.AreEqual(title, tasks.First().title);
+        }
+    }
+}
---
+++ Task3_AppiumDesktopTestsNew/DesktopTests.cs
@@ -0,0 +1,89 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace AppiumWindowsTests
+{
+	public class TaskBoardAppiumWindowsTests
+	{
+		private WindowsDriver<WindowsElement> driver;
+		private const string appiumServer = "http://127.0.0.1:4723/wd/hub";
+		private const string app = @"C:\work\TaskBoard.DesktopClient.exe";
+		private AppiumOptions options;
+
+		[SetUp]
+		public void OpenApp()
+		{
+			this.options = new AppiumOptions() { PlatformName = "Windows" };
+			options.AddAdditionalCapability("app", app);
+			this.driver = new WindowsDriver<WindowsElement>(new Uri(appiumServer), options);
+		}
+
+		[TearDown]
+		public void ShutDownApp()
+		{
+			this.driver.Quit();
+		}
+
+		[Test]
+		public void Test_TaskBoard_Tasks_Lists()
+		{
+			var coonectButton = driver.FindElementByAccessibilityId("buttonConnect");
+			coonectButton.Click();
+
+			var searchField = driver.FindElementByAccessibilityId("textBoxSearchText");
+			searchField.Click();
+			searchField.SendKeys("Project skeleton");
+			var searchButton = driver.FindElementByAccessibilityId("buttonSearch");
+			searchButton.Click();
+
+
+			var openTable = driver.FindElementsByAccessibilityId("listViewTasks");
+
+			foreach (var task in openTable)
+			{
+				if (task.Text.StartsWith("Project"))
+				{
+					Assert.That(task.Text, Is.EqualTo("Project skeleton"));
+					break;
+				}
+			}
+
+		}
+
+		[Test]
+		public void Test_TaskBoard_Add_NewTask_ValidData()
+		{
+			var coonectButton = driver.FindElementByAccessibilityId("buttonConnect");
+			coonectButton.Click();
+
+			var addButton = driver.FindElementByAccessibilityId("buttonAdd");
+			addButton.Click();
+
+			var titleTextBox = driver.FindElementByAccessibilityId("textBoxTitle");
+			var uniqueTitle = "NewTaskTitle" + DateTime.Now.Ticks.ToString();
+			titleTextBox.SendKeys(uniqueTitle);
+
+			var createButton = driver.FindElementByAccessibilityId("buttonCreate");
+			createButton.Click();
+
+			var textBoxSearch = driver.FindElementByAccessibilityId("textBoxSearchText");
+			textBoxSearch.SendKeys(uniqueTitle);
+
+			var searchButton = driver.FindElementByAccessibilityId("buttonSearch");
+			searchButton.Click();
+
+			var openTable = driver.FindElementsByAccessibilityId("listViewTasks");
+
+			foreach (var task in openTable)
+			{
+				if (task.Text.StartsWith("NewTask"))
+				{
+					Assert.That(task.Text, Is.EqualTo(uniqueTitle));
+					break;
+				}
+			}
+		}
+	}
+}
